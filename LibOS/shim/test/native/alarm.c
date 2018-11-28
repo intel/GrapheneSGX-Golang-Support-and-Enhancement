ad8936fcb9d417dd0dd59e989ce865395ac48448
Isaku Yamahata <isaku.yamahata@gmail.com>
2018-11-27 18:43:38 -0800

    shim/test/native/alarm: add alarm_loop and update alarm for more test

--- LibOS/shim/test/native/alarm.c
+++ LibOS/shim/test/native/alarm.c
@@ -2,16 +2,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#include <time.h>
 
 void handler (int signal)
 {
-    printf("alarm goes off\n");
+    int a;
+    printf("alarm goes off %p\n", &a);
+    fflush(stdout);
 }
 
 int main(int argc, char ** argv)
 {
     signal(SIGALRM, &handler);
+    //alarm(100 * 100);
+    //sleep(3);
+    struct timespec t = {
+        .tv_sec = 3,
+        .tv_nsec = 0
+    };
+    printf("&t %p\n", &t);
+    fflush(stdout);
     alarm(1);
-    sleep(3);
+    nanosleep(&t, NULL);
+    printf("done exiting\n");
+    fflush(stdout);
     return 0;
 }
---
+++ LibOS/shim/test/native/alarm_loop.c
@@ -0,0 +1,50 @@
+/* -*- mode:c; c-file-style:"k&r"; c-basic-offset: 4; tab-width:4; indent-tabs-mode:nil; mode:auto-fill; fill-column:78; -*- */
+/* vim: set ts=4 sw=4 et tw=78 fo=cqt wm=0: */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <time.h>
+
+volatile bool loop = true;
+volatile int count = 0;
+
+void handler (int signal)
+{
+    count++;
+    printf("alarm goes off count = %d\n", count);
+    fflush(stdout);
+    loop = false;
+    alarm(1);
+    if (count % 2) {
+        time_t begin = time(NULL);
+        time_t now = time(NULL);
+        while (now - begin < 3) {
+#if 1
+            for (volatile unsigned long i = 0; i < 10000000UL; i++) {
+                /* do nothing */
+            }
+#endif
+            now = time(NULL);
+        }
+    } else {
+        sleep(3);
+    }
+}
+
+int main(int argc, char ** argv)
+{
+    signal(SIGALRM, &handler);
+
+    alarm(1);
+    sleep(3);
+
+    alarm(1);
+    while (loop) {
+        ;
+    }
+    printf("done exiting\n");
+    return 0;
+}
