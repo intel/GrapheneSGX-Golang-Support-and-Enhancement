937c7c7804441b24bafe637cd51408c006785a58
Isaku Yamahata <isaku.yamahata@gmail.com>
2019-01-24 14:29:50 -0800

    [LibOS] shim_futex.c minor update
    - race condition on FUTEX_WAIT
    - type fix

--- LibOS/shim/src/sys/shim_futex.c
+++ LibOS/shim/src/sys/shim_futex.c
@@ -63,7 +63,7 @@ int shim_do_futex (int * uaddr, int op, int val, void * utime,
     uint32_t val2 = 0;
     int ret = 0;
 
-    if (!uaddr || ((uintptr_t) uaddr % sizeof(unsigned int)))
+    if (!uaddr || ((uintptr_t) uaddr % sizeof(int)))
         return -EINVAL;
 
     create_lock_runtime(&futex_list_lock);
@@ -184,8 +184,6 @@ int shim_do_futex (int * uaddr, int op, int val, void * utime,
             /* DEP 1/28/17: Should return ETIMEDOUT, not EAGAIN, on timeout. */
             if (ret == -EAGAIN)
                 ret = -ETIMEDOUT;
-            if (ret == -ETIMEDOUT)
-                LISTP_DEL(&waiter, &futex->waiters, list);
             lock(&hdl->lock);
             /* Chia-Che 10/17/17: FUTEX_WAKE should remove the waiter
              * from the list; if not, we should remove it now. */
