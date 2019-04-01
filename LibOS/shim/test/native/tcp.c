3153e7dd8b25271cca426da7b035bc95c700f177
Isaku Yamahata <isaku.yamahata@gmail.com>
2019-04-01 15:06:00 -0700

    [LibOS] test/native/tcp: allow server to bind ephemeral port and misc clean up
    This patch cleans up tcp.c and also allows server to bind ephemeral port.
    By allowing ephemeral port, it's confinement to run the test repeatedly to
    avoid address in use error.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

--- LibOS/shim/test/native/tcp.c
+++ LibOS/shim/test/native/tcp.c
@@ -1,5 +1,6 @@
 /* copied from http://www.daniweb.com/software-development/c/threads/179814 */
 
+#include <stdbool.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,9 +16,8 @@
 #define SRV_BIND_IP "0.0.0.0"
 #define SRV_IP "127.0.0.1"
 #define PORT 9930
-#define BUFLEN 512
-#define NPACK 10
 
+in_port_t port = PORT;
 const char * fname;
 
 enum { SINGLE, PARALLEL } mode = PARALLEL;
@@ -27,36 +27,60 @@ int pipefds[2];
 
 int server(void)
 {
-    int conn, create_socket, new_socket, fd;
+    int create_socket, new_socket, fd;
     socklen_t addrlen;
-    int bufsize = 1024;
+    size_t bufsize = 1024;
     char * buffer = malloc(bufsize);
     struct sockaddr_in address;
 
-    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0)
-        printf("The socket was created\n");
+    create_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (create_socket < 0) {
+        perror("server socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("server: The socket was created\n");
+
+    if (mode == PARALLEL) {
+        int optval = 0;
+        if (setsockopt(create_socket, SOL_IP, IP_BIND_ADDRESS_NO_PORT,
+                       &optval, sizeof(optval)) < 0) {
+            perror("setcokopt");
+            exit(EXIT_FAILURE);
+        }
+        port = 0;
+    }
 
     address.sin_family = AF_INET;
     inet_pton(AF_INET, SRV_BIND_IP, &(address.sin_addr));
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(port);
 
     if (bind(create_socket, (struct sockaddr *) &address,
              sizeof(address)) < 0) {
         perror("bind");
         close(create_socket);
-        exit(-1);
+        exit(EXIT_FAILURE);
+    }
+
+    if (mode == PARALLEL) {
+        socklen_t len = sizeof(address);
+        if (getsockname(
+                create_socket, (struct sockaddr *) &address, &len) < 0) {
+            perror("getsockname");
+            exit(EXIT_FAILURE);
+        }
+        port = ntohs(address.sin_port);
+        printf("server: port %d\n", port);
     }
 
     if (listen(create_socket, 3) < 0) {
         perror("listen");
         close(create_socket);
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     if (mode == PARALLEL) {
         close(pipefds[0]);
-        char byte = 0;
-        write(pipefds[1], &byte, 1);
+        write(pipefds[1], &port, sizeof(port));
     }
 
     addrlen = sizeof(address);
@@ -66,13 +90,21 @@ int server(void)
     if (new_socket < 0) {
         perror("accept");
         close(create_socket);
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
-    close(create_socket);
+    if (close(create_socket) < 0) {
+        perror("close(create_socket)");
+        exit(EXIT_FAILURE);
+    }
 
     if (do_fork) {
-        if (fork() > 0) {
+        pid_t pid = fork();
+        if (pid) {
+            perror("server fork");
+            exit(EXIT_FAILURE);
+        }
+        if (pid > 0) {
             close(new_socket);
             wait(NULL);
             return 0;
@@ -80,75 +112,112 @@ int server(void)
     }
 
     char buff[18] = {'\0'};
-    if (inet_ntop(AF_INET, &address.sin_addr, buff, 18) <= 0)
+    if (inet_ntop(AF_INET, &address.sin_addr, buff, 18) <= 0) {
         perror("address conversion");
+        exit(EXIT_FAILURE);
+    }
 
-    printf("The client %s is connected...\n", buff);
+    printf("server: The client %s is connected...\n", buff);
 
     if ((fd = open(fname, O_RDONLY, 0)) < 0) {
         perror("file open failed");
         close(new_socket);
         close(create_socket);
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
-    while ((conn = read(fd, buffer, bufsize)) > 0)
-        sendto(new_socket, buffer, conn, 0, 0, 0);
+    while (true) {
+        size_t conn = read(fd, buffer, bufsize);
+        if (conn < 0) {
+            perror("server read");
+            exit(EXIT_FAILURE);
+        }
+        if (conn == 0)
+            break;
+        if (sendto(new_socket, buffer, conn, 0, 0, 0) < 0) {
+            perror("server sendto");
+            exit(EXIT_FAILURE);
+        }
+    }
 
-    printf("Request completed\n");
+    printf("server: Request completed\n");
 
     close(new_socket);
     if (do_fork)
-        exit(0);
+        exit(EXIT_SUCCESS);
     return 0;
 }
 
 int client(void)
 {
-    int count, create_socket;
-    int bufsize = 1024;
+    int create_socket;
+    size_t bufsize = 1024;
     char * buffer = malloc(bufsize);
     struct sockaddr_in address;
 
     if (mode == PARALLEL) {
         close(pipefds[1]);
-        char byte = 0;
-        read(pipefds[0], &byte, 1);
+        read(pipefds[0], &port, sizeof(port));
     }
 
-    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) >= 0)
-        printf("The Socket was created\n");
+    create_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (create_socket < 0) {
+        perror("client socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("client: The Socket was created\n");
 
     address.sin_family = AF_INET;
     inet_pton(AF_INET, SRV_IP, &address.sin_addr);
-    address.sin_port = htons(PORT);
+    address.sin_port = ntohs(port);
+    printf("client: port %d\n", port);
     if (connect(create_socket, (struct sockaddr *) &address,
                 sizeof(address)) == 0) {
-        printf("The connection was accepted with the server\n");
+        printf("client: The connection was accepted with the server\n");
     } else {
-        perror("accept");
-        exit(0);
+        perror("connect");
+        exit(EXIT_SUCCESS);
     }
 
     if (do_fork) {
-        if (fork() > 0) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("client fork");
+            exit(EXIT_FAILURE);
+        }
+        if (pid > 0) {
             close(create_socket);
             wait(NULL);
             return 0;
         }
     }
 
-    printf("Content:\n");
+    printf("client: Content:\n");
 
-    while((count=recv(create_socket, buffer, bufsize, 0))>0)
-        write(1, buffer, count);
+    while (true) {
+        size_t count = recv(create_socket, buffer, bufsize, 0);
+        if (count < 0) {
+            perror("client recv");
+            exit(EXIT_FAILURE);
+        }
+        if (count == 0)
+            break;
 
-    printf("EOF\n");
+        count = write(1, buffer, count);
+        if (count < 0) {
+            perror("client write");
+            exit(EXIT_FAILURE);
+        }
+        if (count == 0)
+            break;
+    }
+
+    printf("client: EOF\n");
 
     buffer[0] = 0;
     close(create_socket);
     if (do_fork)
-        exit(0);
+        exit(EXIT_SUCCESS);
     return 0;
 }
 
@@ -165,13 +234,13 @@ int main(int argc, char ** argv)
         if (strcmp(argv[1], "client") == 0) {
             mode = SINGLE;
             client();
-            return 0;
+            return EXIT_SUCCESS;
         }
 
         if (strcmp(argv[1], "server") == 0) {
             mode = SINGLE;
             server();
-            return 0;
+            return EXIT_SUCCESS;
         }
 
         if (strcmp(argv[1], "fork") == 0) {
@@ -181,9 +250,16 @@ int main(int argc, char ** argv)
     }
     else {
 old:
-        pipe(pipefds);
+        if (pipe(pipefds) < 0) {
+            perror("pipe");
+            return EXIT_FAILURE;
+        }
 
         int pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            return EXIT_FAILURE;
+        }
 
         if (pid == 0)
             client();
@@ -193,5 +269,5 @@ old:
         }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
