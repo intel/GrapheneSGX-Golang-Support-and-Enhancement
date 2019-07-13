yamahata/graphene

local: debug message in Pal/Linux-SGX
Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

local: [Pal/Linux-SGX] fix debug message of sgx_ocall_sleep() SEGV

Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

Pal/src/host/Linux-SGX/sgx_enclave.c
@@ -604,6 +604,7 @@ static int sgx_ocall_sleep(void * pms)
     ms_ocall_sleep_t * ms = (ms_ocall_sleep_t *) pms;
     int ret;
     ODEBUG(OCALL_SLEEP, ms);
+    SGX_DBG(DBG_E, "sgx_ocall_sleep %d\n", ms->ms_microsec);
     if (!ms->ms_microsec) {
         INLINE_SYSCALL(sched_yield, 0);
         return 0;

intel/GrapheneSGX-Golang-Support-and-Enhancement

Pal/src/host/Linux-SGX/sgx_enclave.c
@@ -586,6 +586,7 @@ static int sgx_ocall_sleep(void * pms)
     ms_ocall_sleep_t * ms = (ms_ocall_sleep_t *) pms;
     int ret;
     ODEBUG(OCALL_SLEEP, ms);
+    SGX_DBG(DBG_E, "sgx_ocall_sleep %ld\n", ms->ms_microsec);
     if (!ms->ms_microsec) {
         INLINE_SYSCALL(sched_yield, 0);
         return 0;