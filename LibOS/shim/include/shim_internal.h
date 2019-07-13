yamahata/graphene

[LibOS] use hlt when abort for debug instead of int3
Use hlt instruction for SGX debug.

Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

LibOS/shim/include/shim_internal.h
@@ -166,7 +166,8 @@ static inline void do_pause (void);
 #if USE_PAUSE == 1
 # define pause() do { do_pause(); } while (0)
 #else
-# define pause() do { asm volatile ("int $3"); } while (0)
+//# define pause() do { asm volatile ("int $3"); } while (0)
+# define pause() do { asm volatile ("hlt"); } while (0)
 #endif
 
 #define bug()                                                               \

intel/GrapheneSGX-Golang-Support-and-Enhancement

LibOS/shim/include/shim_internal.h
@@ -169,7 +169,8 @@ static inline void do_pause (void);
 #if USE_PAUSE == 1
 # define PAUSE() do { do_pause(); } while (0)
 #else
-# define PAUSE() do { __asm__ volatile ("int $3"); } while (0)
+//# define PAUSE() do { __asm__ volatile ("int $3"); } while (0)
+# define PAUSE() do { __asm__ volatile ("hlt"); } while (0)
 #endif
 
 #define BUG()                                                               \