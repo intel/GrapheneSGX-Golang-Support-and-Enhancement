yamahata/graphene

[Pal/Linux-SGX] _DkEventWaitTimeout() doesn't return
_DkEventWaitTimeout() of Pal/Linux-SGX doesn't return. ETIMEDOUT and
ETRAYAGAIN are different meaing for futex(WAIT). but
unix_to_pal_error() combines into PAL_ERROR_TRYAGAIN. futex ocall
doesn't provide enough info.
So make ocall_futex() return unix error number and have
the callers of ocall_futex, e.g.  _DkEventWaitTimedout(), convert unix errno
into PAL ERROR NO.

Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

Pal/src/host/Linux-SGX/sgx_enclave.c
@@ -237,6 +237,10 @@ static int sgx_ocall_create_process(void * pms)
     return 0;
 }
 
+/* sgx_ocall_futex return unix errno instead of PAL_ERROR
+ * because PAL_ERROR doesn't distinguish ETIMEDOUT and EAGAIN.
+ * In futex case, they needs to be distinguished.
+ */
 static int sgx_ocall_futex(void * pms)
 {
     ms_ocall_futex_t * ms = (ms_ocall_futex_t *) pms;
@@ -248,9 +252,12 @@ static int sgx_ocall_futex(void * pms)
         ts->tv_sec = ms->ms_timeout / 1000000;
         ts->tv_nsec = (ms->ms_timeout - ts->tv_sec * 1000000) * 1000;
     }
+    SGX_DBG(DBG_E, "futext %p op %d val %d timeout %ld\n",
+            ms->ms_futex, ms->ms_op, ms->ms_val, ms->ms_timeout);
     ret = INLINE_SYSCALL(futex, 6, ms->ms_futex, ms->ms_op, ms->ms_val,
                          ts, NULL, 0);
-    return IS_ERR(ret) ? unix_to_pal_error(ERRNO(ret)) : ret;
+    SGX_DBG(DBG_E, "futext ret %d\n", ret);
+    return ret;
 }
 
 static int sgx_ocall_socketpair(void * pms)

intel/GrapheneSGX-Golang-Support-and-Enhancement

Pal/src/host/Linux-SGX/sgx_enclave.c
@@ -271,6 +271,10 @@ static int sgx_ocall_create_process(void * pms)
     return 0;
 }
 
+/* sgx_ocall_futex return unix errno instead of PAL_ERROR
+ * because PAL_ERROR doesn't distinguish ETIMEDOUT and EAGAIN.
+ * In futex case, they needs to be distinguished.
+ */
 static int sgx_ocall_futex(void * pms)
 {
     ms_ocall_futex_t * ms = (ms_ocall_futex_t *) pms;
@@ -282,6 +286,8 @@ static int sgx_ocall_futex(void * pms)
         ts->tv_sec = ms->ms_timeout / 1000000;
         ts->tv_nsec = (ms->ms_timeout - ts->tv_sec * 1000000) * 1000;
     }
+    SGX_DBG(DBG_E, "futex %p op %d val %d timeout %ld\n",
+            ms->ms_futex, ms->ms_op, ms->ms_val, ms->ms_timeout);
     ret = INLINE_SYSCALL(futex, 6, ms->ms_futex, ms->ms_op, ms->ms_val,
                          ts, NULL, 0);
     ODEBUG_RV(OCALL_FUTEX, ret);