yamahata/graphene

[LibOS/test/native] add test program, cpuid.c to test cpuid
Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

LibOS/shim/test/native/cpuid.c
@@ -1,0 +1,28 @@
+#include <stdio.h>
+
+static inline void native_cpuid(unsigned int *eax, unsigned int *ebx,
+                                unsigned int *ecx, unsigned int *edx)
+{
+    /* ecx is often an input as well as an output. */
+    asm volatile("cpuid"
+        : "=a" (*eax),
+          "=b" (*ebx),
+          "=c" (*ecx),
+          "=d" (*edx)
+        : "0" (*eax), "2" (*ecx));
+}
+
+int main(int argc, char **argv)
+{
+    unsigned eax, ebx, ecx, edx;
+
+    eax = 1; /* processor info and feature bits */
+    native_cpuid(&eax, &ebx, &ecx, &edx);
+
+    printf("stepping %d\n", eax & 0xF);
+    printf("model %d\n", (eax >> 4) & 0xF);
+    printf("family %d\n", (eax >> 8) & 0xF);
+    printf("processor type %d\n", (eax >> 12) & 0x3);
+    printf("extended model %d\n", (eax >> 16) & 0xF);
+    printf("extended family %d\n", (eax >> 20) & 0xFF);
+}

intel/GrapheneSGX-Golang-Support-and-Enhancement

LibOS/shim/test/native/cpuid.c
@@ -1,0 +1,28 @@
+#include <stdio.h>
+
+static inline void native_cpuid(unsigned int *eax, unsigned int *ebx,
+                                unsigned int *ecx, unsigned int *edx)
+{
+    /* ecx is often an input as well as an output. */
+    __asm__ volatile("cpuid"
+                     : "=a" (*eax),
+                       "=b" (*ebx),
+                       "=c" (*ecx),
+                       "=d" (*edx)
+                     : "0" (*eax), "2" (*ecx));
+}
+
+int main(int argc, char **argv)
+{
+    unsigned eax, ebx, ecx, edx;
+
+    eax = 1; /* processor info and feature bits */
+    native_cpuid(&eax, &ebx, &ecx, &edx);
+
+    printf("stepping %d\n", eax & 0xF);
+    printf("model %d\n", (eax >> 4) & 0xF);
+    printf("family %d\n", (eax >> 8) & 0xF);
+    printf("processor type %d\n", (eax >> 12) & 0x3);
+    printf("extended model %d\n", (eax >> 16) & 0xF);
+    printf("extended family %d\n", (eax >> 20) & 0xFF);
+}