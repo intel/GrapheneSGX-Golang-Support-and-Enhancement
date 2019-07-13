yamahata/graphene

[Pal/Linux-SGX] record host tid in enclave for debug
For debug, pass host thread id to enclave, then record it.

Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

Pal/src/host/Linux-SGX/db_main.c
@@ -126,7 +126,7 @@ static int loader_filter (const char * key, int len)
 extern void * enclave_base;
 
 void pal_linux_main(const char ** arguments, const char ** environments,
-                    struct pal_sec * sec_info)
+                    struct pal_sec * sec_info, uint64_t host_tid)
 {
     PAL_HANDLE parent = NULL;
     unsigned long start_time = _DkSystemTimeQuery();
@@ -225,6 +225,7 @@ void pal_linux_main(const char ** arguments, const char ** environments,
     SET_ENCLAVE_TLS(sig_stack_high,
                     GET_ENCLAVE_TLS(sig_stack_offset) + enclave_base +
                     ENCLAVE_SIG_STACK_SIZE);
+    SET_ENCLAVE_TLS(common.host_tid, host_tid);
 
     /* call main function */
     pal_main(pal_sec.instance_id, manifest, exec,

intel/GrapheneSGX-Golang-Support-and-Enhancement

Pal/src/host/Linux-SGX/db_main.c
@@ -196,7 +196,7 @@ extern void * enclave_top;
 
 void pal_linux_main(char * uptr_args, uint64_t args_size,
                     char * uptr_env, uint64_t env_size,
-                    struct pal_sec * uptr_sec_info)
+                    struct pal_sec * uptr_sec_info, uint64_t host_tid)
 {
     /*
      * Our arguments are comming directly from the urts. We are responsible to
@@ -388,6 +388,7 @@ void pal_linux_main(char * uptr_args, uint64_t args_size,
     first_thread->thread.tcs =
         enclave_base + GET_ENCLAVE_TLS(tcs_offset);
     SET_ENCLAVE_TLS(thread, (__pal_control.first_thread = first_thread));
+    SET_ENCLAVE_TLS(common.host_tid, host_tid);
 
     /* call main function */
     pal_main(pal_sec.instance_id, manifest, exec,